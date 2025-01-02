bb29f14c81276eeeb71aa81f3a844a5ed5b30a6b
Christoph Cernusca <cernusca.c22@htlwienwest.at>
2025-01-02 10:59:28 +0100

    Added main script using viewer functionalities

--- main.py
+++ main.py
@@ -4,4 +4,17 @@ Main module
 Program Entry Point
 """
 
+from scripts.geometry import GeoCircle as circle
+from scripts.geometry import GeoRectangle as rect
+import scripts.viewer as view
 
+if __name__ == "__main__":
+    view.add_viewer(150, 75, 0, 100, 100, 200)
+    view.add_viewer(450, 200, 180, 200, 100, 100)
+
+    view.add_geometry(325, 75, (255, 0, 0, 150), circle(0, 0, 50))
+    view.add_geometry(325, 325, (0, 0, 255, 255), circle(0, 0, 50), rect(50, 0, 100, 100))
+    view.add_geometry(75, 325, (0, 255, 0, 255), rect(0, 0, 100, 100))
+    view.add_geometry(375, 75, (255, 255, 0, 255), circle(0, 0, 50))
+
+    view.start()
